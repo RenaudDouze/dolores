7c4850b7f6fbdf09de74f9bf81865c58b9bb4995
Renaud Lerognon <renaud.lerognon@gmail.com>
2016-01-02 11:42:09 +0100

    [++][ADD][App] Add printable view

--- web/js/src/view/AppView.js
+++ web/js/src/view/AppView.js
@@ -6,4 +6,32 @@ var AppView = ParentView.extend({
 		table = new TableModel();
 		tableView = new TableView({model: table});
 	},
+
+    /**
+        Events
+    **/
+    events: {
+        "click .action-print": "printVersion",
+        "click .action-unprint": "unprintVersion",
+    },
+
+    /**
+     * Show a more printable version
+     */
+    printVersion: function() {
+        $('.hidden-print').hide();
+        $('.visible-print').show();
+        $('.action-unprint').show();
+        $('.action-print').hide();
+    },
+
+    /**
+     * Show a the normal version
+     */
+    unprintVersion: function() {
+        $('.hidden-print').show();
+        $('.visible-print').hide();
+        $('.action-unprint').hide();
+        $('.action-print').show();
+    },
 });
